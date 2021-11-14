a2105d6f7af01b4068fb79fe3f45b6c9d75e00e7
mdarhdarz <84657631+mdarhdarz@users.noreply.github.com>
2021-11-14 19:33:15 +0800

    Add files via upload

--- 1.alpha-beta-pruning/fast_actions.py
+++ 1.alpha-beta-pruning/fast_actions.py
@@ -19,9 +19,6 @@ def fast_kill_action(board):
     action = search_double(board, 1)
     if action is not None:
         return action
-    action = search_double(board, 2)
-    if action is not None:
-        return action
     return None
 
 
@@ -181,88 +178,160 @@ def search_four_op(board, color):
 
 
 def search_double(board, color):
-    # 'double four', 'one four one three' and 'double three'
+    # 'double four', 'four-three' and 'double three'
     boardLength = pp.width
-    # possible placement to achieve four
+
+    # possible actions to achieve four
     # column
-    col4 = set()
+    col4_my = set()
+    col4_op = set()
     for x in range(boardLength - 4):
         for y in range(boardLength):
             pieces = tuple(board[x + d][y] for d in range(5))
             if pieces.count(0) == 2 and pieces.count(color) == 3:
                 for d in range(5):
                     if pieces[d] == 0:
-                        col4.add((x + d, y))
+                        col4_my.add((x + d, y))
+            if pieces.count(0) == 2 and pieces.count(3 - color) == 3:
+                for d in range(5):
+                    if pieces[d] == 0:
+                        col4_op.add((x + d, y))
     # row
-    row4 = set()
+    row4_my = set()
+    row4_op = set()
     for x in range(boardLength):
         for y in range(boardLength - 4):
             pieces = tuple(board[x][y + d] for d in range(5))
             if pieces.count(0) == 2 and pieces.count(color) == 3:
                 for d in range(5):
                     if pieces[d] == 0:
-                        row4.add((x, y + d))
+                        row4_my.add((x, y + d))
+            if pieces.count(0) == 2 and pieces.count(3 - color) == 3:
+                for d in range(5):
+                    if pieces[d] == 0:
+                        row4_op.add((x, y + d))
     # positive diagonal
-    pos4 = set()
+    pos4_my = set()
+    pos4_op = set()
     for x in range(boardLength - 4):
         for y in range(boardLength - 4):
             pieces = tuple(board[x + d][y + d] for d in range(5))
             if pieces.count(0) == 2 and pieces.count(color) == 3:
                 for d in range(5):
                     if pieces[d] == 0:
-                        pos4.add((x + d, y + d))
+                        pos4_my.add((x + d, y + d))
+            if pieces.count(0) == 2 and pieces.count(3 - color) == 3:
+                for d in range(5):
+                    if pieces[d] == 0:
+                        pos4_op.add((x + d, y + d))
     # oblique diagonal
-    ob4 = set()
+    ob4_my = set()
+    ob4_op = set()
     for x in range(boardLength - 4):
         for y in range(4, boardLength):
             pieces = tuple(board[x + d][y - d] for d in range(5))
             if pieces.count(0) == 2 and pieces.count(color) == 3:
                 for d in range(5):
                     if pieces[d] == 0:
-                        ob4.add((x + d, y - d))
+                        ob4_my.add((x + d, y - d))
+            if pieces.count(0) == 2 and pieces.count(3 - color) == 3:
+                for d in range(5):
+                    if pieces[d] == 0:
+                        ob4_op.add((x + d, y - d))
 
-    # possible placement to achieve three
+    # possible actions to achieve three
     # column
-    col3 = set()
+    col3_my = set()
+    col3_op = set()
     for x in range(boardLength - 5):
         for y in range(boardLength):
             pieces = tuple(board[x + d][y] for d in range(6))
             if pieces[0] == 0 and pieces[5] == 0 and pieces[1:5].count(color) == 2 and pieces[1:5].count(0) == 2:
                 for d in range(1, 5):
                     if pieces[d] == 0:
-                        col3.add((x + d, y))
+                        col3_my.add((x + d, y))
+            if pieces[0] == 0 and pieces[5] == 0 and pieces[1:5].count(3 - color) == 2 and pieces[1:5].count(0) == 2:
+                for d in range(1, 5):
+                    if pieces[d] == 0:
+                        col3_op.add((x + d, y))
     # row
-    row3 = set()
+    row3_my = set()
+    row3_op = set()
     for x in range(boardLength):
         for y in range(boardLength - 5):
             pieces = tuple(board[x][y + d] for d in range(6))
             if pieces[0] == 0 and pieces[5] == 0 and pieces[1:5].count(color) == 2 and pieces[1:5].count(0) == 2:
                 for d in range(1, 5):
                     if pieces[d] == 0:
-                        row3.add((x, y + d))
+                        row3_my.add((x, y + d))
+            if pieces[0] == 0 and pieces[5] == 0 and pieces[1:5].count(3 - color) == 2 and pieces[1:5].count(0) == 2:
+                for d in range(1, 5):
+                    if pieces[d] == 0:
+                        row3_op.add((x, y + d))
     # positive diagonal
-    pos3 = set()
+    pos3_my = set()
+    pos3_op = set()
     for x in range(boardLength - 5):
         for y in range(boardLength - 5):
             pieces = tuple(board[x + d][y + d] for d in range(6))
             if pieces[0] == 0 and pieces[5] == 0 and pieces[1:5].count(color) == 2 and pieces[1:5].count(0) == 2:
                 for d in range(1, 5):
                     if pieces[d] == 0:
-                        pos3.add((x + d, y + d))
+                        pos3_my.add((x + d, y + d))
+            if pieces[0] == 0 and pieces[5] == 0 and pieces[1:5].count(3 - color) == 2 and pieces[1:5].count(0) == 2:
+                for d in range(1, 5):
+                    if pieces[d] == 0:
+                        pos3_op.add((x + d, y + d))
     # oblique diagonal
-    ob3 = set()
+    ob3_my = set()
+    ob3_op = set()
     for x in range(boardLength - 5):
         for y in range(5, boardLength):
             pieces = tuple(board[x + d][y - d] for d in range(6))
             if pieces[0] == 0 and pieces[5] == 0 and pieces[1:5].count(color) == 2 and pieces[1:5].count(0) == 2:
                 for d in range(1, 5):
                     if pieces[d] == 0:
-                        ob3.add((x + d, y - d))
+                        ob3_my.add((x + d, y - d))
+            if pieces[0] == 0 and pieces[5] == 0 and pieces[1:5].count(3 - color) == 2 and pieces[1:5].count(0) == 2:
+                for d in range(1, 5):
+                    if pieces[d] == 0:
+                        ob3_op.add((x + d, y - d))
 
-    sets = [col4, row4, pos4, ob4, col3, row3, pos3, ob3]
-    for i in range(8):
-        for j in range(i+1, 8):
-            if j != i+4:
-                intersect = sets[i] & sets[j]
+    sets_my = [col4_my, row4_my, pos4_my, ob4_my, col3_my, row3_my, pos3_my, ob3_my]
+    sets_op = [col4_op, row4_op, pos4_op, ob4_op, col3_op, row3_op, pos3_op, ob3_op]
+    # my double four and three-four
+    for i in range(4):
+        for j in range(i + 1, 4):
+            intersect = sets_my[i] & sets_my[j]
+            if len(intersect) != 0:
+                return list(intersect)[0]
+    for i in range(4):
+        for j in range(4, 8):
+            if j != i + 4:
+                intersect = sets_my[i] & sets_my[j]
                 if len(intersect) != 0:
                     return list(intersect)[0]
+    # op double four and three-four
+    for i in range(4):
+        for j in range(i + 1, 4):
+            intersect = sets_op[i] & sets_op[j]
+            if len(intersect) != 0:
+                return list(intersect)[0]
+    for i in range(4):
+        for j in range(4, 8):
+            if j != i + 4:
+                intersect = sets_op[i] & sets_op[j]
+                if len(intersect) != 0:
+                    return list(intersect)[0]
+    # my double three
+    for i in range(4, 8):
+        for j in range(i + 1, 8):
+            intersect = sets_my[i] & sets_my[j]
+            if len(intersect) != 0:
+                return list(intersect)[0]
+    # op double three
+    for i in range(4, 8):
+        for j in range(i + 1, 8):
+            intersect = sets_op[i] & sets_op[j]
+            if len(intersect) != 0:
+                return list(intersect)[0]
